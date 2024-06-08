0a3fb7e3cebf2c96c2383af431c6b692048bd1b7
程英杰 <116469840+cheng-lao@users.noreply.github.com>
2024-06-08 19:03:17 +0800

    Update reversetcpserver.py

--- reversetcpserver.py
+++ reversetcpserver.py
@@ -1,4 +1,3 @@
-import os
 import argparse
 from threading import Thread
 import socket
@@ -7,7 +6,7 @@ import time
 def argsparse():
     parameters = argparse.ArgumentParser(description="Reverse TCP Server")
     parameters.add_argument("-p","--port",type=int,help="The port of the server",default=8080, required=True)
-    parameters.add_argument("-l","--listen",type=int,help="The number of the server listen",default=5)
+    parameters.add_argument("-l","--listen",type=int,help="The number of the server listen",default=50)
     args = parameters.parse_args()
     return args
 
@@ -22,7 +21,6 @@ def reverseResponseMessage(content: str):
     message = types + str(len(content)).zfill(4) + content
     return message
 
-
 def handle_client(Server:socket.socket):
     global BlockNum
     try:
@@ -30,15 +28,10 @@ def handle_client(Server:socket.socket):
             data = Server.recv(10240)
             data = data.decode()
             types = data[0:2]
-            # content = data[6:]
             if types == "00":
-                # 此时是初始化报文
-                # 生成的报文内容是发送的块数
-                cnt = int(data[2:])
-                # 发送回应报文
+                cnt = int(data[2:]) # 此时是初始化报文， 生成的报文内容是发送的块数
                 message = agreementMessage()
-                Server.send(message.encode())
-                # time.sleep(0.0001)  # 休眠一下 等待发送
+                Server.send(message.encode())   # 发送回应报文
                 break
             else :continue
         for i in range(cnt):
@@ -46,10 +39,8 @@ def handle_client(Server:socket.socket):
             message = message.decode()
             types = message[0:2]
             content = message[6:]
-            # print(f"收到的消息: {content}")
             content = content[::-1]
-            response = reverseResponseMessage(content)
-            # print(f"发送的消息: {content}")
+            response = reverseResponseMessage(content)  # 生成反转报文
             Server.send(response.encode())
             time.sleep(0.001)
     except Exception as e:
@@ -58,21 +49,19 @@ def handle_client(Server:socket.socket):
         print("Close Connection:",Server)
         time.sleep(10)
         Server.close()
-        
-
 
 def main(args):
     global server
     server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     server.bind(("172.19.73.224",args.port))
-    server.listen(args.listen)
+    server.listen(args.listen)  # 最大连接数
     while True:
-        client,addr = server.accept()
-        print(f"Connection from {addr}")
-        ClientPool[addr] = client       # 向池子当中增加一些增加
-        client_handler = Thread(target=handle_client,args=(client,))
-        client_handler.daemon = True
-        client_handler.start()
+        client,addr = server.accept()   # 接受连接
+        print(f"Connection from {addr}")    # 打印连接信息
+        ClientPool[addr] = client       # 向池子当中增加一个客户端
+        client_handler = Thread(target=handle_client,args=(client,))    # 创建一个线程
+        client_handler.daemon = True    # 设置为守护线程
+        client_handler.start()        # 启动线程
         
     
 
@@ -82,9 +71,3 @@ if __name__ == "__main__":
     ClientPool = {}
     args = argsparse()
     main(args)
-
-
-
-
-
-
