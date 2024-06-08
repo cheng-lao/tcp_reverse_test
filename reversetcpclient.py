7cc9ce20120d95dec846cefe73c2f191ba13c648
程英杰 <116469840+cheng-lao@users.noreply.github.com>
2024-06-08 19:00:06 +0800

    Update reversetcpclient.py

--- reversetcpclient.py
+++ reversetcpclient.py
@@ -33,23 +33,20 @@ def IntilizationMessage():
     size = FileSize 
     index = 0
 
-    while size!=0:
+    while size!=0:  # 循环计算总共有多少块 每次生成一个随机大小的块 直到size为0 size一开始是文件内容总大小
         if size < args.MinSizeBlock:
             # 如果小于最小块大小 直接将这部分作为最后的发送部分
             sendList.append(content[index : index + size])
             size = 0
             index = index + size
         else:
-            randomsize = random.randint(args.MinSizeBlock,args.MaxSizeBlock)
-            sendlen.append(randomsize)
+            randomsize = random.randint(args.MinSizeBlock,args.MaxSizeBlock)    # 随机生成块大小
             sendList.append(content[index : index + randomsize])
             size -= randomsize
             index = index + randomsize
     N = len(sendList)
-    print("发送的总块数是 ",N)
+    print("发送的总块数是 ",N)  # 打印总块数
     time.sleep(1)
-    sendlen.append(args.MinSizeBlock)
-    sendlen.append(args.MaxSizeBlock)
     message = tyeps + str(N).zfill(4)
     return message
 
@@ -72,7 +69,8 @@ def reverserRequestMessage(idx: int):
 def GetReverseFilePath(filepath:str):
     filename = os.path.basename(filepath)
     path = os.path.dirname(filepath)
-    ReversePath = os.path.join(path,"reverse_"+filename)
+    ReversePath = os.path.join(path,"reverse_"+filename)    # 反转后的文件路径
+    # 如果文件存在则在文件名后面加上数字 例如 reverse_1test.txt 
     if not os.path.exists(ReversePath):
         with open(ReversePath, 'w') as f:
             pass
@@ -90,23 +88,21 @@ def GetReverseFilePath(filepath:str):
 
 def run(client:socket,args):
     global ReceivedContent
-    filepath = args.filepath
-    ReversePath = GetReverseFilePath(filepath)
+    filepath = args.filepath       # 获取文件路径
+    ReversePath = GetReverseFilePath(filepath)  # 获取反转后的文件路径
     try:
         for i in range(len(sendList)):
-            message = reverserRequestMessage(i)
+            message = reverserRequestMessage(i) # 生成反转请求报文
             client.send(message.encode())
             response = client.recv(10240)
-            response = response.decode()
-            # content = MessageTackle(response)
-            content = response[6:]
-            # types = response[0:2]
-            print(f"第{i+1}块反转之后收到的的内容: {content}")
-            ReceivedContent += content
+            response = response.decode()        
+            content = response[6:]      
+            print(f"第{i+1}块反转之后收到的的内容: {content}")  
+            ReceivedContent += content      # 将收到的内容拼接起来
     except ConnectionResetError as e:
-        print("连接被重置",e) 
+        print("连接被重置",e)   
     finally :
-        content = ReceivedContent
+        content = ReceivedContent   # 将拼接好的内容赋值给content 一次性写入文件
         with open(ReversePath, 'r+') as f:
             old = f.read()
             f.seek(0)
@@ -118,27 +114,23 @@ def run(client:socket,args):
 def main(args):
     GetContent(args.filepath)
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # TCP客户端连接
-    client.connect((args.serverip, args.port))
-    message = IntilizationMessage()
-    client.send(message.encode())
-    response = client.recv(10240)
-    response = response.decode()
-    if response == agreementMessage :
-        print("Iitialization Success")
-    run(client,args)
+    client.connect((args.serverip, args.port))  # 连接服务器
+    message = IntilizationMessage() # 生成初始化报文
+    client.send(message.encode())   # 发送初始化报文
+    response = client.recv(10240)   # 接收初始化报文
+    response = response.decode()    # 解码
+    if response == agreementMessage :   # 如果收到的报文是协议报文
+        print("Iitialization Success")  # 初始化成功
+    run(client,args)        # 运行主程序
 
 
 if __name__ == "__main__":
-    start = time.time()
+    start = time.time()     # 计时开始
     FileSize = 0
     content = ""
     sendList = []
-    sendlen = []
-    ReceivedContent = ""
-    args = argsparse()
-    main(args)
-    endtime = time.time()
+    ReceivedContent = ""    # 初始化全局变量
+    args = argsparse()    # 参数解析
+    main(args)  # 主程序
+    endtime = time.time()   # 计时结束
     print("The Program Total time is ",endtime-start)
-
-
-
